9d9d69bbc2bd455ba63faa630344674826e81c1f
DmitriyKholin <105742934+DmitriyKholin@users.noreply.github.com>
2023-03-20 17:43:44 +0300

    commit

--- frontend/src/app/_guards/auth.guard.ts
+++ frontend/src/app/_guards/auth.guard.ts
@@ -12,8 +12,7 @@ export class AuthGuard implements CanActivate {
     constructor(
         private router: Router,
         private userService: UserService
-    ) {
-    }
+    )
 
     canActivate(
         route: ActivatedRouteSnapshot,
